cac02ed039c85764eaeccf2ba806c13dc483bfb9
luisfernandospoljaic <luisfernandospoljaric@gmail.com>
2026-09-22 16:32:16 -0300

    pbe1-crud

---
+++ ds/seduc/2des/02-lima/aula07/revisao/ex2/script.js
@@ -0,0 +1,39 @@
+function verificarNumeros(){
+
+    let numeros = []
+
+    numeros.push(Number(document.getElementById("numero1").value))
+    numeros.push(Number(document.getElementById("numero2").value))
+    numeros.push(Number(document.getElementById("numero3").value))
+    numeros.push(Number(document.getElementById("numero4").value))
+    numeros.push(Number(document.getElementById("numero5").value))
+
+    let maior = numeros[0]
+    let menor = numeros[0]
+
+    let soma = 0;
+    let pares = 0;
+    let impares = 0;
+
+    for (let i = 0; i < numeros.length; i++){
+        let numero = numeros[i]
+
+        soma = soma + numero;
+
+        if(numero > maior){
+            maior = numero
+        }
+
+        if(numero < menor){
+            menor = numero
+        }
+
+    }
+
+
+
+}
+
+
+
+
